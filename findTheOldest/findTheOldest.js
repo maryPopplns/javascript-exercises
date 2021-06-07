a1d07779fd272b20fe570d41922dcae588df658d
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-07 12:22:16 -0700

    completed oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function () {
+  return arr
+    .map((e) => {
+      return {
+        name: e.name,
+        age: e.yearOfDeath
+          ? e.yearOfDeath - e.yearOfBirth
+          : new Date().getFullYear() - e.yearOfBirth,
+      };
+    })
+    .sort((a, b) => a.age - b.age)
+    .pop();
 };
 
 module.exports = findTheOldest;

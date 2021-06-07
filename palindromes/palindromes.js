6b0e2c620a419d4f4e9373f4900653642f715c99
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-07 13:50:50 -0700

    completed removeFromArray

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+let palindromes = function (str1) {
+  let stripped = str1
+    .replaceAll(".", "")
+    .replaceAll(",", "")
+    .replaceAll("!", "")
+    .replaceAll(" ", "")
+    .toLowerCase();
+  return stripped.split("").reverse().join("") === stripped;
 };
 
 module.exports = palindromes;
--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (input, ...args) {
+  argumentList = [];
+  for (let i = 0; i < [...args].length; i++) {
+    argumentList.push([...args][i]);
+  }
+  return input.filter((e) => !argumentList.includes(e));
 };
 
 module.exports = removeFromArray;

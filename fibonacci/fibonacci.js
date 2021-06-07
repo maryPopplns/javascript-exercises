103b7050a89137e60a80c5caeea106adfe76cd22
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-07 11:30:52 -0700

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+  let fibbSeq = [1, 1];
+  for (let i = 0; fibbSeq.length < input; i++) {
+    fibbSeq.push(fibbSeq[i] + fibbSeq[i + 1]);
+  }
+  return input > 0 ? fibbSeq.pop() : "OOPS";
 };
 
 module.exports = fibonacci;

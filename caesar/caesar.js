2129a452a3ebe5d25d03417ef5cf45d3473400d8
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-07 11:18:17 -0700

    completed caesar && calculator

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,41 @@
-const caesar = function() {
-
+const caesar = function (str, shift) {
+  let cypher = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 122 || str.charCodeAt(i) < 65) {
+      cypher.push(str.charCodeAt(i));
+    } else if (str[i].toLowerCase() == str[i]) {
+      let converted = str.charCodeAt(i) + shift;
+      if (converted < 97) {
+        converted = 122 - (96 - converted);
+      }
+      if (converted > 122) {
+        converted = converted - 122 + 96;
+      }
+      if (converted > 122) {
+        converted = converted - 122 + 96;
+      }
+      if (converted > 122) {
+        converted = converted - 122 + 96;
+      }
+      cypher.push(converted);
+    } else {
+      let converted = str.charCodeAt(i) + shift;
+      if (converted < 65) {
+        converted = 90 - (64 - converted);
+      }
+      if (converted > 90) {
+        converted = converted - 90 + 64;
+      }
+      if (converted > 90) {
+        converted = converted - 90 + 64;
+      }
+      if (converted > 90) {
+        converted = converted - 90 + 64;
+      }
+      cypher.push(converted);
+    }
+  }
+  return cypher.map((e) => String.fromCharCode(e)).join("");
 };
 
 module.exports = caesar;

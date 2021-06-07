3da619b2e4c93922b8598a338f91514b82b8046f
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-07 14:13:41 -0700

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0) {
+    return "ERROR";
+  }
 
+  let big, small;
+  if (a > b) {
+    big = a;
+    small = b;
+  } else {
+    big = b;
+    small = a;
+  }
+  let arr = [];
+  for (let i = small; i <= big; i++) {
+    arr.push(i);
+  }
+  return arr.reduce((a, e) => a + e, 0);
 };
 
 module.exports = sumAll;
